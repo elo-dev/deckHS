ca8ecb5ca7f6bae92d73ded2f51782e9f0f5353f
Dima Frolov <dimasiys25@gmail.com>
2021-01-17 16:36:36 +0300

    Function create newDeck

--- script.js
+++ script.js
@@ -35,14 +35,72 @@ openTabs('.demons_deck', '.col_demons');
 openTabs('.animals_deck', '.col_animals');
 openTabs('.mehs_deck', '.col_mehs');
 
+// ЦИКЛ ДОБАВЛЕНИЯ КАРТИНОК НА СТРАНИЦУ
+
 let selectEntity = document.getElementById("selectEntity"),
     add = "";
     for (let i = 1; i <= 41; ++i) {
     add += `<li>
-                <input type="checkbox" id="checkBoxAnimals${i}" />
+                <input type="checkbox" id="checkBoxAnimals${i}" value="${i}" />
                 <label id="selectEntityImg" for="checkBoxAnimals${i}">
-                    <img src="allEntity/entity${i}.png">
+                    <img src="allEntity/entity${i}.png" data-img="allEntity/entity${i}.png">
                 </label>
             </li>`;
 }
 selectEntity.innerHTML = add;
+
+// СОЗДАНИЕ ДЕКИ
+
+const btn = document.querySelector('.btn-success');
+
+btn.addEventListener('click', function(){
+    var userDeck = document.getElementById('userDeck');
+    var firstLi = userDeck.getElementsByTagName('P')[0];
+    
+    var name = document.querySelector('.nameDeck').value;
+
+    if(typeof nameCheckbox === 'string' && name.length !== 0){
+        var nameArchetype = document.createElement('P');
+        var nameDeck = document.createElement('H1');
+        nameArchetype.innerHTML = 'Дека:' + nameCheckbox;
+        nameDeck.innerHTML = name;
+        userDeck.insertBefore(nameArchetype, firstLi);
+        userDeck.insertBefore(nameDeck, firstLi);
+    }
+
+    // block.childNodes.forEach(el => {el.remove()});
+    arr.forEach(el => {
+        let img = document.createElement('img');
+        img.classList.add('img_userDeck');
+        img.setAttribute('src', el);
+        block.insertBefore(img, firstLi);
+    })
+
+    checkbox.checked = false;
+    document.querySelector('.nameDeck').value = '';
+});
+
+
+// ВЫВОД НАЗВАНИЯ ЧЕКБОКСА
+
+var listCheckbox = document.querySelectorAll('[type="radio"]');
+var nameCheckbox;
+var checkbox;
+        listCheckbox.forEach(e =>{
+            e.onchange = function (event){
+                nameCheckbox = event.target.nextElementSibling.textContent;
+                checkbox = event.target;
+            };
+        });
+
+// ДОБАВЛЕНИЕ КАРТИНКИ В МАССИВ
+
+let img = document.querySelectorAll('[src]');
+let block = document.getElementById('userDeck');
+let arr = [];
+
+img.forEach(el => {
+    el.onclick = function (event){
+        arr.push(event.target.getAttribute('data-img'));
+    }
+});
